59f1ae430fdd131d060f66b0fbaa32b5eba90a64
Baiyuan Wang <bywang@thoughtworks.com>
2018-08-21 19:16:16 +0800

    first commit

---
+++ slider-ad/slider-ad.js
@@ -0,0 +1,26 @@
+// libs/slider-ad.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    ads: Array
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    autoplay: true,
+    interval: 5000,
+    duration: 500,
+    showdots: true
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
